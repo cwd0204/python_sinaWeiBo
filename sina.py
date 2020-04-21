097b9ec025d7dcd9a2dd19f93594f6bb3a9df6f8
cwd0204 <cwd0204@126.com>
2020-04-21 17:34:54 +0800

    2 to 3
    2 to 3

--- sina.py
+++ sina.py
@@ -10,7 +10,7 @@ def get_access_token(app_key, app_secret, callback_url):
     client = APIClient(app_key=app_key, app_secret=app_secret, redirect_uri=callback_url)
     # 获取授权页面网址
     auth_url = client.get_authorize_url()
-    print auth_url
+    print(auth_url)
 
     # 在浏览器中访问这个URL，会跳转到回调地址，回调地址后面跟着code，输入code
     code = raw_input("Input code:")
@@ -18,8 +18,8 @@ def get_access_token(app_key, app_secret, callback_url):
     access_token = r.access_token
     # token过期的UNIX时间
     expires_in = r.expires_in
-    print 'access_token:', access_token
-    print 'expires_in:', expires_in
+    print('access_token:', access_token)
+    print('expires_in:', expires_in)
 
     return access_token, expires_in
 def init_login():
@@ -44,12 +44,12 @@ def send_pic(client,picpath,message):
     mes = message.decode('utf-8')
     client.statuses.upload.post(status=mes, pic=f)
     f.close()  # APIClient不会自动关闭文件，需要手动关闭
-    print u"发送成功！"
+    print(u"发送成功！")
 
 def send_mes(client,message):
     utext = unicode(message,"UTF-8")
     client.post.statuses__update(status=utext)
-    print u"发送成功！"
+    print(u"发送成功！")
 
 
 if __name__ == '__main__':
